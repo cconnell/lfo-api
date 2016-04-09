948d439383c1555716522f071eed53f783426e9f
Chris Connell <chris.g.connell@gmail.com>
2016-04-09 14:41:20 -0500

    added tests

--- lib/customer_scoring.rb
+++ lib/customer_scoring.rb
@@ -3,11 +3,21 @@ require 'unirest'
 
 module CustomerScoring
   class Customer
-    def self.score
-      api_array = Unirest.get("").body 
-      return OpenStruct.new(api_array)
+     def self.score(income, zipcode, age)
+          api_response_hash = Unirest.get("http://not_real.com/customer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}").body 
+        http_response = api_response_hash.code
+        if http_response == 200
+          response_body = api_response_hash.body
+          return OpenStruct.new(response_body)
+        else
+            if http_response.between?(400, 499)
+              "#{http_response} error - Please verify your paramaters"
+            elsif http_response.between?(500, 599)
+              "#{http_response} error - Please try again.  If error continues contact administrator."
+            else
+              "Please try again."
+            end
+        end  
     end
   end
 end
-
-
--- spec/customer_scoring_spec.rb
+++ spec/customer_scoring_spec.rb
@@ -4,8 +4,23 @@ describe CustomerScoring do
   it 'has a version number' do
     expect(CustomerScoring::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'gets an http response' do
+    expect(CustomerScoring::Customer.score("x", "y", "z")).not_to be nil
   end
+  it 'does not get an http response' do
+    expect(CustomerScoring::Customer.score("x", "y", "z")).not_to be false
+  end
+
+  it 'returns an error message if score info not found' do
+    expect(CustomerScoring::Customer.score("x", "y", "z")).to eq "404 error - Please verify your paramaters"
+  end
+
+  it 'requires more than two arguments' do
+    expect{CustomerScoring::Customer.score("x", "y")}.to raise_error(ArgumentError)
+  end
+
+   it 'requires less than four arguments' do
+    expect{CustomerScoring::Customer.score("x", "y", "z", "zz")}.to raise_error(ArgumentError)
+  end
+
 end
