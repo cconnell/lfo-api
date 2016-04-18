e8fb155fad07c8b3e0b7d523767e4afe7b68b594
Chris Connell <chris.g.connell@gmail.com>
2016-04-17 20:32:00 -0500

    added argument validations and tests

--- lib/customer_scoring.rb
+++ lib/customer_scoring.rb
@@ -3,21 +3,43 @@ require 'unirest'
 
 module CustomerScoring
   class Customer
-     def self.score(income, zipcode, age)
-          api_response_hash = Unirest.get("http://not_real.com/customer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}")
+    def self.score(income, zipcode, age)
+      errors = []      
+
+      if income != income.scan(/\d+/)[0]
+        errors << "Invalid income: #{income}"
+      end
+
+      if zipcode != zipcode.scan(/^\d{5}/)[0]
+          errors << "Invalid zip code: #{zipcode}"
+      end
+
+      if age == age.scan(/\d+/)[0]
+        unless (0..130).include?(age.to_i)
+          errors << "Invalid age: #{age}"
+        end
+      else 
+        errors << "Invalid age: #{age}"
+      end
+
+      if errors.length > 0
+        return errors
+      else
+        api_response_hash = Unirest.get("http://yahoo.com/customer_scoring?income=#{income}&zipcode=#{zipcode}&age=#{age}")
         http_response = api_response_hash.code
-        if http_response == 200
-          response_body = api_response_hash.body
-          return OpenStruct.new(response_body)
-        else
+          if http_response == 200
+            response_body = api_response_hash.body
+            return OpenStruct.new(response_body)
+          else
             if http_response.between?(400, 499)
-              "#{http_response} error - Please verify your paramaters"
+                "#{http_response} error - Please verify your paramaters"
             elsif http_response.between?(500, 599)
-              "#{http_response} error - Please try again.  If error continues contact administrator."
+                "#{http_response} error - Please try again.  If error continues contact administrator."
             else
-              "Something isn't right... please try again."
+                "Something isn't right... please try again."
             end
-        end  
+          end
+      end    
     end
   end
 end
--- spec/customer_scoring_spec.rb
+++ spec/customer_scoring_spec.rb
@@ -1,17 +1,7 @@
 require 'spec_helper'
 
 describe CustomerScoring do
-  it 'gets an http response' do
-    expect(CustomerScoring::Customer.score("x", "y", "z")).not_to be nil
-  end
-  it 'does not get an http response' do
-    expect(CustomerScoring::Customer.score("x", "y", "z")).not_to be false
-  end
-
-  it 'returns an error message if score info not found' do
-    expect(CustomerScoring::Customer.score("x", "y", "z")).to eq "404 error - Please verify your paramaters"
-  end
-
+  
   it 'requires more than two arguments' do
     expect{CustomerScoring::Customer.score("x", "y")}.to raise_error(ArgumentError)
   end
@@ -19,5 +9,33 @@ describe CustomerScoring do
    it 'requires less than four arguments' do
     expect{CustomerScoring::Customer.score("x", "y", "z", "xx")}.to raise_error(ArgumentError)
   end
+
+  it 'validates income argument' do
+    expect(CustomerScoring::Customer.score("x35000", "90210", "50")).to eq ["Invalid income: x35000"]
+  end
+
+  it 'validates zipcode argument' do
+    expect(CustomerScoring::Customer.score("35000", "9021", "50")).to eq ["Invalid zip code: 9021"]
+  end
+
+  it 'validates age argument' do
+    expect(CustomerScoring::Customer.score("35000", "90210", "300")).to eq ["Invalid age: 300"]
+  end
+
+  it 'validates income, age, and zipcode arguments' do
+    expect(CustomerScoring::Customer.score("x", "y", "z")).to eq ["Invalid income: x", "Invalid zip code: y", "Invalid age: z"]
+  end
+
+  it 'gets an http response' do
+    expect(CustomerScoring::Customer.score("0", "00000", "1")).not_to be nil
+  end
+  it 'does not get an http response' do
+    expect(CustomerScoring::Customer.score("0", "00000", "1")).not_to be false
+  end
+
+  it 'returns an error message if score info not found' do
+    expect(CustomerScoring::Customer.score("0", "00000", "1")).to eq "404 error - Please verify your paramaters"
+  end
+
 
 end
